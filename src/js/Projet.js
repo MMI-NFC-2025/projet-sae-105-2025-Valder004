f1f370aada2457f82b724d6d60c50c0405c03310
Valder004 <valentin.deroo@edu.univ-fcomte.fr>
2026-01-06 10:45:37 +0100

    debut css

---
+++ src/js/Projet.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('header button');
+const menu = document.querySelector('.menu');
+
+// Ajouter un événement au clic du bouton
+button.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+});
+
+// Fermer le menu quand on clique sur un lien
+document.querySelectorAll('.nav__link').forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('menu--open');
+  });
+});

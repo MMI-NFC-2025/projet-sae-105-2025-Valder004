b7e2ecfa639788b723860206769f220e3f75e898
Valder004 <valentin.deroo@edu.univ-fcomte.fr>
2026-01-07 12:41:02 +0100

    css

--- src/js/Projet.js
+++ src/js/Projet.js
@@ -12,3 +12,22 @@ document.querySelectorAll('.nav__link').forEach(link => {
     menu.classList.remove('menu--open');
   });
 });
+
+
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+// Largeur de défilement d’un item
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: -260, behavior: "smooth" });
+  });
+
+  // Scroll au clic sur le bouton suivant
+nextButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: +260, behavior: "smooth" });
+});
+}
